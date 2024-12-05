3cfeec04caa422d031b192f6f21915afcb9ea392
Tony-Elia <antony.safaga@gmail.com>
2024-12-05 15:20:59 -0500

    counter UI

--- app/src/main/java/com/example/acomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/acomposeapp/MainActivity.kt
@@ -1,79 +1,72 @@
 package com.example.acomposeapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.collection.mutableIntSetOf
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.getValue
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text2.input.TextFieldCharSequence
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import  androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.acomposeapp.ui.theme.AComposeAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AComposeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        
-                    )
-                }
+                Scaffold(modifier = Modifier.fillMaxSize()){
+                    Counter() }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var name by rememberSaveable { mutableStateOf("") }
-    Column (
-        modifier = Modifier.fillMaxSize(),
+fun Counter() {
+    val counter by rememberSaveable{ mutableIntStateOf(0) }
+    Column(
+        modifier =Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Hello. $name!",
-            modifier = modifier
-        )
-        OutlinedTextField(
-            value = name,
-            onValueChange = { n ->
-                name = n
-            },
-            modifier = modifier.padding(16.dp),
-            label = { Text("Name") }
-        )
+        horizontalAlignment= Alignment.CenterHorizontally
+    ){
+        Button(onClick = {}) {
+            Text("Increase")
+        }
+        Text(counter.toString(),
+            fontSize=64.sp)
+        Button(onClick = {}) {
+            Text("Decrease")
+        }
+
+
     }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AComposeAppTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
-        }   
+
     }
 }
